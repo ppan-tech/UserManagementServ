f33459d9936cc05fb797626d5fc6a5db4f322a62
Puneet Pant <punepant@gmail.com>
2025-09-21 17:09:26 -0700

    modified Request mapping and annotation for signup-api

--- src/main/java/com/swiftmart/usermgmtservice/controllers/UserController.java
+++ src/main/java/com/swiftmart/usermgmtservice/controllers/UserController.java
@@ -4,20 +4,21 @@ import com.swiftmart.usermgmtservice.dtos.SignUpRequestDTO;
 import com.swiftmart.usermgmtservice.dtos.UserDTO;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController//means this class will handle HTTP requests and return responses in a RESTful manner
 // @RequestMapping("/api/auth")//base URL path for all endpoints in this controller
-
+@RequestMapping("/users")
 public class UserController {
 
     //public void signup(String name, String email, String password){
     //public User signup(SignUpRequestDTO signUpRequestDTO) {
     //public UserDTO signup(SignUpRequestDTO signUpRequestDTO) {
 
-    @RequestMapping("/users/signup")
+    @PostMapping("/signup")
     public ResponseEntity<UserDTO> signup(@RequestBody SignUpRequestDTO signUpRequestDTO) {
         return null;
     }
