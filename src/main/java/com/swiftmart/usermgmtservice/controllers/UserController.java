ce32e4687bd2499c05082684f9ff0575efbc8bf1
Puneet Pant <punepant@gmail.com>
2025-09-21 17:25:14 -0700

    added RequestBody annotation for loginReq DTO

--- src/main/java/com/swiftmart/usermgmtservice/controllers/UserController.java
+++ src/main/java/com/swiftmart/usermgmtservice/controllers/UserController.java
@@ -4,12 +4,10 @@ import com.swiftmart.usermgmtservice.dtos.LoginRequestDTO;
 import com.swiftmart.usermgmtservice.dtos.SignUpRequestDTO;
 import com.swiftmart.usermgmtservice.dtos.TokenDTO;
 import com.swiftmart.usermgmtservice.dtos.UserDTO;
+import lombok.Getter;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController//means this class will handle HTTP requests and return responses in a RESTful manner
 // @RequestMapping("/api/auth")//base URL path for all endpoints in this controller
@@ -27,7 +25,7 @@ public class UserController {
 
     @PostMapping("/login")//Note:Login should be post as it will generate a token.So it will return a TokenDTO.
     //public void login(String email, String pas){
-    public TokenDTO login(LoginRequestDTO requestDTO) {
+    public TokenDTO login(@RequestBody LoginRequestDTO requestDTO) {
         return null;
     }
 
