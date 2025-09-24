a84f1a84323e7a50a16423a077d2cd54af609974
Puneet Pant <punepant@gmail.com>
2025-09-24 13:57:23 -0700

    added ID generation scheme at DB level

--- src/main/java/com/swiftmart/usermgmtservice/models/BaseModel.java
+++ src/main/java/com/swiftmart/usermgmtservice/models/BaseModel.java
@@ -1,5 +1,7 @@
 package com.swiftmart.usermgmtservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -12,8 +14,15 @@ import java.util.Date;
 @MappedSuperclass
 public abstract class BaseModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date createdAt;
     private Date lastUpdatedAt;
     //private boolean deleted;
+    /*
+        @GeneratedValue: This is an optional but very common annotation. It tells the persistence
+                provider how the primary key value should be generated.
+                In this example, GenerationType.IDENTITY means the database itself will auto-increment
+                the id for new records. This is a common practice for primary keys.
+     */
 }
