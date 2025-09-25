6332fe68fbc1db3d85d8653d8f62d32173e9d00d
Puneet Pant <punepant@gmail.com>
2025-09-24 17:17:13 -0700

    added code in UserServiceImpl for signup functionality

--- src/main/java/com/swiftmart/usermgmtservice/services/UserServiceImpl.java
+++ src/main/java/com/swiftmart/usermgmtservice/services/UserServiceImpl.java
@@ -2,12 +2,33 @@ package com.swiftmart.usermgmtservice.services;
 
 import com.swiftmart.usermgmtservice.models.Token;
 import com.swiftmart.usermgmtservice.models.User;
+import com.swiftmart.usermgmtservice.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public User signup(String name, String email, String password) {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if(optionalUser.isPresent()){
+            return optionalUser.get();
+        }
+        //But if email is not present in DBthen create a new user and save it to DB.
+        User user = new User();
+        user.setEmail(email);
+        user.setName(name);
+        user.setPassword(password);//Note:In real world app we should hash the password before saving.
+
+        //TODO: Hence lets go to maven repo and add dependency for BCrypt.
+
+        //userRepository.save(new User(name,email,password));
         return null;
     }
 
